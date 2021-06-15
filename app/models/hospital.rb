1b0181ac57dad9dd32fe873e74b31215dcab3ed5
Jacq Whitmer <jacqwhitmer@gmail.com>
2021-06-15 13:21:46 -0400

    Refactor AR method in doctor model

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -2,6 +2,6 @@ class Hospital < ApplicationRecord
   has_many :doctors
 
   def unique_universities
-    Doctor.where('hospital_id = ?', self.id).select(:university).distinct(:university)
+    Doctor.where(hospital_id: self.id).select(:university).distinct(:university)
   end
 end
